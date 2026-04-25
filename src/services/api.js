fb76a9a2e663f1ddc0e63da0c77500f0bdf6a728
Aniketkadam876 <aniketkadam0708@gmail.com>
2026-04-25 14:02:51 +0530

    Add API URL

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,7 @@
 import axios from "axios";
 
+const API_URL = process.env.REACT_APP_API_URL;
+
 export default axios.create({
-  baseURL: "https://disease-backend-oxhh.onrender.com",
+  baseURL: API_URL,
 });
